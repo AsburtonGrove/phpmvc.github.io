703f182d34f35a805b03f7abc14ab53c4c27cd7f
AsburtonGrove <abang_jonathan@yahoo.com>
2021-12-24 01:58:10 +0700

    first commit

---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class User_model {
+    private $nama = 'Bryan';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container mt-4">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL;?>/img/bryan.jpeg" alt="Bryan" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun , saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+    </div>
